5ce6a9fc7824c27be44f0a8f6222eca56704f642
LoretoBustos <loretobustoscampos@gmail.com>
2023-07-31 21:32:53 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+
+alert ("hola g43");
+
+console.log("introducción");
+
+
+let hola = 5;
+let chao = 10;
+let resultado = 5 + 10
+console.log( resultado)
+
+
+
+let a = 2;
+let b = 3;
+let c = 4;
+let total = 2+3+4;
+alert( a + 5 );
+alert( 5 + c);
+alert(total);
+alert( b- a);
+
+let saludo = "hola!!!!";
+let despedida = " chao!!!";
+alert (saludo + despedida);
+
+let caja = prompt("agregue el nombre d la caja");
+let cantidad = prompt("agregar cantidad");
+let valor = 400;
+let totalCajas = caja + cantidad;
+alert (totalCajas)
+
+let precio = 400
+let precioOferta = document.querySelector("cantidad");
+precioOferta.innerHTML = precioOferta.toLocaleString();
